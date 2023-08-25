dd1828b8b9e5dbef4888488c8d5a652b07c69664
Dan Klco <klco@adobe.com>
2023-08-25 11:34:26 -0400

    Adding color coding

--- src/components/FlameGraph.vue
+++ src/components/FlameGraph.vue
@@ -6,15 +6,30 @@ import * as d3 from 'd3';
 const props = defineProps(['parsed']);
 let chart;
 onMounted(() => {
+
+  var details = document.getElementById('flame-graph--details');
   chart = flamegraph()
-    .width(1200)
+    .width(1050)
     .height(400)
-    .transitionDuration(750)
-    .selfValue(false);
+    .setColorMapper((el, originalColor) => {
+      if (el.highlight) {
+        return originalColor
+      };
+      if (el.data.name.includes('/libs') || el.data.name.includes('com.day') || el.data.name.includes('com.adobe')) {
+        return '#f68d2e';
+      }
+      if (el.data.name.includes('org.apache') || ['handleSecurity', 'ResourceResolution', 'ServletResolution'].includes(el.data.name)) {
+        return '#D22128'
+      }
+      if (el.data.name.includes('/apps') || el.data.name.includes('/mnt') || el.data.name.includes('/conf') || el.data.name.includes('/content') || el.data.name.includes('com.')) {
+        return '#0dcaf0';
+      }
+
+      return '#adb5bd';
+    });
   d3.select('#flame-graph')
     .datum(props.parsed.requestTree.children[0])
     .call(chart);
-  var details = document.getElementById('flame-graph--details');
   chart.setDetailsElement(details);
 });
 
@@ -32,24 +47,17 @@ function search() {
 
 <template>
   <nav class="navbar navbar-light bg-light">
-    <button
-      class="btn btn-outline-success my-2 my-sm-0"
-      @click="chart.resetZoom()"
-    >
+    <button class="btn btn-outline-success my-2 my-sm-0" @click="chart.resetZoom()">
       Reset Zoom
     </button>
 
     <div class="pull-right">
-      <form
-        class="row row-cols-lg-auto g-3 align-items-center"
-        @submit="
-          (event) => {
-            event.stopPropagation();
-            event.preventDefault();
-            emit('search', term);
-          }
-        "
-      >
+      <form class="row row-cols-lg-auto g-3 align-items-center" @submit="(event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        emit('search', term);
+      }
+        ">
         <div class="form-group">
           <a class="btn" @click="chart.clear()">Clear</a>
         </div>
@@ -62,6 +70,46 @@ function search() {
       </form>
     </div>
   </nav>
-  <div id="flame-graph"></div>
-  <p>Element: <span id="flame-graph--details"></span></p>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div id="flame-graph"></div>
+        <p>Element: <span id="flame-graph--details"></span></p>
+      </div>
+      <div class="col-2">
+        <h3>Color Key</h3>
+        <ul class="list-unstyled">
+          <li>
+            <span class="color-block" style="background-color: #f68d2e;"></span>
+            AEM/Adobe
+          </li>
+          <li>
+            <span class="color-block" style="background-color: #D22128;"></span>
+            Apache Sling
+          </li>
+          <li>
+            <span class="color-block" style="background-color: #0dcaf0;"></span>
+            Customer / Extension
+          </li>
+          <li>
+            <span class="color-block" style="background-color: #adb5bd;"></span>
+            Other
+          </li>
+        </ul>
+
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.color-block {
+  display: inline-block;
+  height: 20px;
+  width: 20px;
+}
+
+.col {
+  overflow: auto;
+}
+</style>
